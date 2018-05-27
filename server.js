9c6f8f37bddc8ba232556c6ab7988f931569702c
Luke Kwerneland <lkwerneland@gmail.com>
2018-05-27 13:36:23 -0600

    database update

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 
-const express = require ('express');
-const exphbs = require ('express-handlebars');
+const express = require('express');
+const exphbs = require('express-handlebars');
 // body-parsing middleware;
-const bodyParser = require ('body-parser');
-const path = require ('path');
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
+const db = require("./models");
 
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.use(express.static("public"));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-const PORT =3000;
+const PORT = 3000;
 app.listen(PORT);
-return console.log ("I hear you barkin dawg " + PORT);
+return console.log("I hear you barkin dawg " + PORT);
 
+// Routes
+// =============================================================
+require("./routes/apiroutes.js")(app);
+require("./routes/htmlroutes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function () {
+    app.listen(PORT, function () {
+    });
+});
 // Syntx to connect to Postgresql server
 // CONNECT TO connection_target [ AS connection_name ] [ USER connection_user ]
 // CONNECT TO DEFAULT
