8dd16ba93a6a93a32a0ee2e4c26f88fcbbf40752
Luke Kwerneland <lkwerneland@gmail.com>
2018-05-29 20:21:45 -0600

    models update

--- models/bikes.js
+++ models/bikes.js
@@ -1,21 +1,70 @@
-module.exports = function(sequelize, DataTypes) {
-    const Bikes = sequelize.define("Bikes", {
-      text: {
-        type: DataTypes.STRING,
-        // AllowNull is a flag that restricts a bike from being entered if it doesn't
-        // have a text value
-        allowNull: true,
-        // len is a validation that checks that our bike info is between 1 and 140 characters
-        validate: {
-          len: [1, 140]
-        }
-      },
-      complete: {
-        type: DataTypes.BOOLEAN,
-        // defaultValue is a flag that defaults a new bikes complete value to false if
-        // it isn't supplied one
-        defaultValue: false
+module.exports = function (sequelize, DataTypes) {
+  const Bikes = sequelize.define("Bikes", {
+    Make: {
+      type: DataTypes.STRING,
+      // AllowNull is a flag that restricts a bike from being entered if it doesn't
+      // have a text value
+      allowNull: true,
+      // len is a validation that checks that our bike info is between 1 and 140 characters
+      validate: {
+        len: [1, 140]
       }
+    },
+    Model: {
+      type: DataTypes.STRING,
+      // AllowNull is a flag that restricts a bike from being entered if it doesn't
+      // have a text value
+      allowNull: true,
+      // len is a validation that checks that our bike info is between 1 and 140 characters
+      validate: {
+        len: [1, 140]
+      }
+    },
+
+    Color: {
+      type: DataTypes.STRING,
+      // AllowNull is a flag that restricts a bike from being entered if it doesn't
+      // have a text value
+      allowNull: true,
+      // len is a validation that checks that our bike info is between 1 and 140 characters
+      validate: {
+        len: [1, 140]
+      }
+    },
+
+    SerialNumber: {
+      type: DataTypes.STRING,
+      // AllowNull is a flag that restricts a bike from being entered if it doesn't
+      // have a text value
+      allowNull: true,
+      // len is a validation that checks that our bike info is between 1 and 140 characters
+      validate: {
+        len: [1, 140]
+      }
+    },
+
+    ImageURL: {
+      type: DataTypes.STRING,
+      // AllowNull is a flag that restricts a bike from being entered if it doesn't
+      // have a text value
+      allowNull: true,
+      // len is a validation that checks that our bike info is between 1 and 140 characters
+      validate: {
+        len: [1, 140]
+      }
+    }
     });
-    return Bikes;
+    
+    Bikes.associate = function(models) {
+      // We're saying that a Bike should belong to an User
+      // A Bike can't be created without an User due to the foreign key constraint
+      Bikes.belongsTo(models.User, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+    };
+  
+    return Post;
   };
+  
