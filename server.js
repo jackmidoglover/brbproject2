959461f09595fe8a552531b60b1b938ef12b9fe6
Amanda Ogonosky <amandaogonosky@Os-MacBook-Air.local>
2018-05-31 20:38:07 -0600

    remove body from main add images

--- server.js
+++ server.js
@@ -1,37 +1,19 @@
 
-const express = require('express');
-const exphbs = require('express-handlebars');
+const express = require ('express');
+const exphbs = require ('express-handlebars');
 // body-parsing middleware;
-const bodyParser = require('body-parser');
-const path = require('path');
+const bodyParser = require ('body-parser');
+const path = require ('path');
 const app = express();
-const db = require("./models");
-const methodOverride = require('method-override');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(bodyParser.json());
-app.use(express.static("public"));
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-var authroutes = require('./controllers/passport_controllers.js');
 
-
-const PORT = 8080;
-
-app.use(authroutes);
-app.use(methodOverride('_method'));
-
-
-// Routes
-// =============================================================
-
-// Syncing our sequelize models and then starting our Express app
-// =============================================================
-db.sequelize.sync({ force: true }).then(function () {
-    app.listen(PORT, function () {
-        console.log("I hear you barkin dawg", PORT)
-    });
-});
+const PORT =3000;
+app.listen(PORT);
+return console.log ("I hear you barkin dawg " + PORT);
 
 
 
