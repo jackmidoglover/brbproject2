e4ff095f31691ea2e8a4fabe25f7b8be15529716
Luke Kwerneland <lkwerneland@gmail.com>
2018-06-01 18:51:16 -0600

    new push

--- models/user.js
+++ models/user.js
@@ -1,68 +1,62 @@
 const bcrypt = require('bcrypt');
-module.exports = function(sequelize, DataTypes) {
-    const Users = sequelize.define("Users", {
-      // Giving the User model a name of type STRING 
+module.exports = function (sequelize, DataTypes) {
+  const Users = sequelize.define("Users", {
+    // Giving the User model a name of type STRING 
     UserName: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false,
-        validate: {
-            len: [1, 140]
-          }
-    },    
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        len: [1, 140]
+      }
+    },
     Name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: [1, 140]
-          }
-    },    
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 140]
+      }
+    },
     UserPassword: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: [1, 60]
-          }
-    },    
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 60]
+      }
+    },
     Zipcode: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: [1, 140]
-          }
-    },    
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 140]
+      }
+    },
     Email: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false,
-        validate: {
-            len: [1, 140]
-          }
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        len: [1, 140]
+      }
     }
+  });
+
+
+
+
+  Users.associate = function (models) {
+    // Associating User with Bikes
+    // When an User is deleted, also delete any associated Bikes
+    Users.hasMany(models.Bikes, {
+      onDelete: "cascade"
     });
-
-    
-
-
-    Users.associate = function(models) {
-      // Associating User with Bikes
-      // When an User is deleted, also delete any associated Bikes
-      Users.hasMany(models.Bikes, {
-        onDelete: "cascade"
-      });
-    };
-
-    
-
-    Users.prototype.validatePassword = function(password){
-      return bcrypt.compareSync(password, this.password);
-    }
-    
-    //delete User
-  app.delete('/api/user', (req, res) => {
-    Users.remove(req.body)
-      .then(user => res.json(user))
-  })
-
-    return Users;
   };
+
+
+
+  Users.prototype.validatePassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+  }
+
+  return Users;
+};
